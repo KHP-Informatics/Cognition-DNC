85fbd28241cb3f1c7f7901ec44c95e60d92cc37b
Richard Jackson <k1223140@kcl.ac.uk>
2015-11-02 16:58:47 +0000

    more progress on full integration test

--- pipeline/src/test/java/uk/ac/kcl/iop/brc/core/pipeline/dncpipeline/data/FullPipelineIntegrationTests.java
+++ pipeline/src/test/java/uk/ac/kcl/iop/brc/core/pipeline/dncpipeline/data/FullPipelineIntegrationTests.java
@@ -11,8 +11,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.hibernate.Query;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.junit.After;
@@ -21,6 +23,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import spark.utils.IOUtils;
 import uk.ac.kcl.iop.brc.core.pipeline.common.testutils.IntegrationTest;
+import uk.ac.kcl.iop.brc.core.pipeline.dncpipeline.service.DNCPipelineService;
 
 /**
  *
@@ -30,64 +33,80 @@ public class FullPipelineIntegrationTests extends IntegrationTest{
     private static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(FullPipelineIntegrationTests.class);
     @Autowired
     private PatientDao patientDao;
+    
+    @Autowired
+    private DNCPipelineService service;
 
     @Before
     public void initDb() {
-        patientDao.executeSQLQueryForSource("create table tblPatient(ID int, nhs_no varchar(100), dob date, PRIMARY KEY (ID))");
-        patientDao.executeSQLQueryForSource("create table tblPatientNames(first_name varchar(100), last_name varchar(100), patient_id int)");
-        patientDao.executeSQLQueryForSource("create table dateOfBirths(patient_id int, dob date)");
-        patientDao.executeSQLQueryForSource("create table nhsNumbers(patient_id int, nhs_no varchar(100))");        
-        patientDao.executeSQLQueryForSource("create table tblPatientAddresses(Address1 varchar(100), postcode varchar(100), patient_id int)");
-        patientDao.executeSQLQueryForSource("create table tblPatientPhoneNumbers(number varchar(100), patient_id int)");
-        patientDao.executeSQLQueryForSource("create table tblPatientCarers(first_name varchar(100), last_name varchar(100), patient_id int)");
-        patientDao.executeSQLQueryForSource("create table testDocs(patient_id int, doc_id int, doc VARBINARY(100) )");        
-        patientDao.executeSQLQueryForSource("create view testCoordinateView AS SELECT patient_id, 'testDocs', 'doc', doc_id, 'binary',null FROM testDocs");
-
+        patientDao.executeSQLQueryForSource("CREATE TABLE tblPatient(ID int, nhs_no varchar(100), dob date, PRIMARY KEY (ID))");
+        patientDao.executeSQLQueryForSource("CREATE TABLE tblPatientNames(first_name varchar(100), last_name varchar(100), patient_id int)");
+        patientDao.executeSQLQueryForSource("CREATE TABLE tblDateOfBirths(patient_id int, dob date)");
+        patientDao.executeSQLQueryForSource("CREATE TABLE tblNhsNumbers(patient_id int, nhs_no varchar(100))");        
+        patientDao.executeSQLQueryForSource("CREATE TABLE tblPatientAddresses(Address1 varchar(100), postcode varchar(100), patient_id int)");
+        patientDao.executeSQLQueryForSource("CREATE TABLE tblPatientPhoneNumbers(number varchar(100), patient_id int)");
+        patientDao.executeSQLQueryForSource("CREATE TABLE tblPatientCarers(first_name varchar(100), last_name varchar(100), patient_id int)");
+        patientDao.executeSQLQueryForSource("CREATE TABLE tblTestDocs(patient_id int, doc_id int, doc LONGVARBINARY )");        
+        patientDao.executeSQLQueryForSource("CREATE VIEW vwTestCoordinates AS SELECT patient_id AS patientId, 'tblTestDocs' AS sourceTable, 'doc' AS sourceColumn , doc_id AS idInSourceTable, 'doc_id' AS pkColumnName,  'binary' AS type , null AS updatetime FROM tblTestDocs");
+        patientDao.executeSQLQueryForSource("CREATE TABLE tblTestOutputCoordinate ( sourceTable VARCHAR(100), sourceColumn VARCHAR(100) , idInSourceTable INT , processedText VARCHAR(1500000) )");
+        
         insertBinaries(patientDao.createSourceSession()); 
         
                 
-        patientDao.executeSQLQueryForSource("insert into tblPatient values(1, '123123', '1990-05-09')");
-        patientDao.executeSQLQueryForSource("insert into dateOfBirths values(1, '1990-05-09')");
-        patientDao.executeSQLQueryForSource("insert into dateOfBirths values(1, '1990-05-10')");
-        patientDao.executeSQLQueryForSource("insert into nhsNumbers values(1, '123123')");
-        patientDao.executeSQLQueryForSource("insert into nhsNumbers values(1, '444444')");
-        patientDao.executeSQLQueryForSource("insert into tblPatientNames values('michael', 'gregorski', 1)");
-        patientDao.executeSQLQueryForSource("insert into tblPatientNames values('micha', 'gregor', 1)");
-        patientDao.executeSQLQueryForSource("insert into tblPatientAddresses values('address1', 'cb4 2za', 1)");
-        patientDao.executeSQLQueryForSource("insert into tblPatientAddresses values('address2', 'cb1 2za', 1)");
-        patientDao.executeSQLQueryForSource("insert into tblPatientPhoneNumbers values('213123', 1)");
-        patientDao.executeSQLQueryForSource("insert into tblPatientPhoneNumbers values('0778', 1)");
-        patientDao.executeSQLQueryForSource("insert into tblPatientCarers values('Richard', 'Jackson', 1)");
-        patientDao.executeSQLQueryForSource("insert into tblPatientCarers values('Ismail', 'Kartoglu', 1)");
+        patientDao.executeSQLQueryForSource("INSERT INTO tblPatient values(1, '123123', '1990-05-09')");
+        patientDao.executeSQLQueryForSource("INSERT INTO tblDateOfBirths values(1, '1990-05-09')");
+        patientDao.executeSQLQueryForSource("INSERT INTO tblDateOfBirths values(1, '1990-05-10')");
+        patientDao.executeSQLQueryForSource("INSERT INTO tblNhsNumbers values(1, '123123')");
+        patientDao.executeSQLQueryForSource("INSERT INTO tblNhsNumbers values(1, '444444')");
+        patientDao.executeSQLQueryForSource("INSERT INTO tblPatientNames values('michael', 'gregorski', 1)");
+        patientDao.executeSQLQueryForSource("INSERT INTO tblPatientNames values('micha', 'gregor', 1)");
+        patientDao.executeSQLQueryForSource("INSERT INTO tblPatientAddresses values('address1', 'cb4 2za', 1)");
+        patientDao.executeSQLQueryForSource("INSERT INTO tblPatientAddresses values('address2', 'cb1 2za', 1)");
+        patientDao.executeSQLQueryForSource("INSERT INTO tblPatientPhoneNumbers values('213123', 1)");
+        patientDao.executeSQLQueryForSource("INSERT INTO tblPatientPhoneNumbers values('0778', 1)");
+        patientDao.executeSQLQueryForSource("INSERT INTO tblPatientCarers values('Richard', 'Jackson', 1)");
+        patientDao.executeSQLQueryForSource("INSERT INTO tblPatientCarers values('Ismail', 'Kartoglu', 1)");
         
     }
 
     @After
     public void dropDb() {
-        patientDao.executeSQLQueryForSource("drop table tblPatient");
-        patientDao.executeSQLQueryForSource("drop table tblPatientNames");
-        patientDao.executeSQLQueryForSource("drop table tblPatientAddresses");
-        patientDao.executeSQLQueryForSource("drop table tblPatientPhoneNumbers");
-        patientDao.executeSQLQueryForSource("drop table tblPatientCarers");
-        patientDao.executeSQLQueryForSource("drop table dateOfBirths");
-        patientDao.executeSQLQueryForSource("drop table nhsNumbers");
+        patientDao.executeSQLQueryForSource("DROP TABLE tblPatient");
+        patientDao.executeSQLQueryForSource("DROP TABLE tblPatientNames");
+        patientDao.executeSQLQueryForSource("DROP TABLE tblDateOfBirths");
+        patientDao.executeSQLQueryForSource("DROP TABLE tblNhsNumbers");
+        patientDao.executeSQLQueryForSource("DROP TABLE tblPatientAddresses");
+        patientDao.executeSQLQueryForSource("DROP TABLE tblPatientPhoneNumbers");
+        patientDao.executeSQLQueryForSource("DROP TABLE tblPatientCarers");
+        patientDao.executeSQLQueryForSource("DROP VIEW vwTestCoordinates");        
+        patientDao.executeSQLQueryForSource("DROP TABLE tblTestDocs");
+        patientDao.executeSQLQueryForSource("DROP TABLE tblTestOutputCoordinate");
     }
     
     @Test
     public void testTest(){
         
+        service.startCreateModeWithDBView();
+        
+        Session session = patientDao.createSourceSession();        
+        Query query = session.createSQLQuery("select * from tblTestOutputCoordinate");
+        List result = query.list();        
+        for(Object ob : result){
+            System.out.println(ob.toString());
+        }
     }
 
     private void insertBinaries(Session session) {        
         try {
-            String sql = "INSERT INTO testDocs (patient_id, doc_id, doc) VALUES(?,?,?)";
-            SQLQuery query = session.createSQLQuery(sql);
+            String sql = "INSERT INTO tblTestDocs (patient_id, doc_id, doc) VALUES(?,?,?)";
             InputStream stream = getClass().getClassLoader().getResourceAsStream("docexample.doc");
-            byte[] binaryData = IOUtils.toByteArray(stream);
+            byte[] binaryData = IOUtils.toByteArray(stream);            
             for(int i =1; i<=20;i++){
+                SQLQuery query = session.createSQLQuery(sql);                
                 query.setInteger(0, 1);
                 query.setInteger(1, i);
                 query.setBinary(2, binaryData);
+                query.executeUpdate();
             }
         } catch (IOException ex) {
             Logger.getLogger(FullPipelineIntegrationTests.class.getName()).log(Level.SEVERE, null, ex);
